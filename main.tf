e5bef649cd42f001a6c0425b564e476a2d9837bb
Jith2 <121929065+Jith2@users.noreply.github.com>
2025-06-18 23:32:38 +0530

    modify the tf files

--- main.tf
+++ main.tf
@@ -30,8 +30,3 @@ terraform {
     use_lockfile   = true
   }
 }
-resource "aws_s3_bucket_policy" "this" {
-  count  = var.bucket_policy != null ? 1 : 0
-  bucket = aws_s3_bucket.this.id
-  policy = var.bucket_policy
-}
